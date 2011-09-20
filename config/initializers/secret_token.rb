3d823da4f5f7e88cb762ec75c27da4a4edafb08d
sionc <sion.chaudhuri@gmail.com>
2011-09-20 01:16:10 -0700

    Initial cimmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Robots::Application.config.secret_token = '02cdf26b1807173f9c08ce49c4d8d5c2758020d5593201632c0881dd142b1f581a5981a5dd6c670edda264c44760835077425e9e83d1ef13a9f3295ae4715794'
