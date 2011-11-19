b63a571e93a76f25b0143a68a8ac87a99b3afa1c
sionc <sion.chaudhuri@gmail.com>
2011-11-18 18:50:41 -0800

    Misc

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,4 @@
+//= require jquery
+//= require jquery_ujs
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
